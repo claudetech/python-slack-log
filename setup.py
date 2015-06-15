80a434640a7f206c72ecf3f03cead1581ecfe228
Daniel Perez <daniel@claudetech.com>
2015-06-15 16:48:25 +0900

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='slack_log_handler',
-    version='0.2.0',
+    version='0.2.1',
     author='Daniel Perez',
     author_email='daniel@claudetech.com',
     packages=['slack_log_handler'],
